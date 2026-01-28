6630b3f017f16feac549fa6643a4d8c2c52799c9
TheRedDice <Maxence.Dorvaux@gmail.com>
2026-01-28 14:28:09 +0100

    Fixed steel ?

---
+++ config/emendatusenigmatica/material/steel.js
@@ -0,0 +1,15 @@
+{
+  "id": "steel",
+  "localizedName": "Steel",
+  "color": "#575757",
+  "processedTypes": [
+    "ingot",
+    "nugget",
+    "storage_block",
+    "dust",
+    "plate",
+    "rod",
+    "gear"
+  ],
+  "strata": []
+}
