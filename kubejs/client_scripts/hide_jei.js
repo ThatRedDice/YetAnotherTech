b179a7969fb200b8439f6a53db0b5d1e7a579fa0
TheRedDice <thereddestdice@gmail.com>
2026-01-28 15:19:27 +0100

    Fixed hide_jei.js

--- kubejs/client_scripts/hide_jei.js
+++ kubejs/client_scripts/hide_jei.js
@@ -1,4 +1,5 @@
-JEIEvents.hideItems(event => {
+RecipeViewerEvents.removeEntries('item', event => {
+    // Liste des matériaux à nettoyer
     const materials = [
         'iron', 'gold', 'copper', 
         'zinc', 'brass', 
@@ -8,55 +9,42 @@ JEIEvents.hideItems(event => {
     ]
 
     materials.forEach(material => {
-        // --- CREATE ---
-        // On ne cache pas les originaux si EE n'existe pas, mais ici on assume que EE couvre tout.
-        if (material !== 'copper' && material !== 'zinc' && material !== 'brass') {
-             // Create n'a pas beaucoup de métaux, mais on cache ce qu'il a en trop si EE le remplace
-        }
-        // Create Specifics (Doublons Plaques/Nuggets)
-        event.hide(`create:${material}_sheet`) 
-        event.hide(`create:${material}_nugget`)
-        // Zinc Create vs Zinc EE : On garde EE
-        if (material === 'zinc') {
-            event.hide('create:zinc_ingot')
-            event.hide('create:raw_zinc')
-            event.hide('create:zinc_nugget')
-            event.hide('create:zinc_block')
-        }
-        if (material === 'brass') {
-            event.hide('create:brass_ingot')
-            event.hide('create:brass_nugget')
-            event.hide('create:brass_block')
-        }
-
         // --- IMMERSIVE ENGINEERING ---
-        event.hide(`immersiveengineering:ingot_${material}`)
-        event.hide(`immersiveengineering:plate_${material}`)
-        event.hide(`immersiveengineering:dust_${material}`)
-        event.hide(`immersiveengineering:nugget_${material}`)
-        event.hide(`immersiveengineering:raw_${material}`)
-        event.hide(`immersiveengineering:storage_${material}`)
-        event.hide(`immersiveengineering:raw_block_${material}`)
-
+        event.remove(`immersiveengineering:ingot_${material}`)
+        event.remove(`immersiveengineering:plate_${material}`)
+        event.remove(`immersiveengineering:dust_${material}`)
+        event.remove(`immersiveengineering:nugget_${material}`)
+        event.remove(`immersiveengineering:raw_${material}`)
+        
         // --- ORITECH ---
-        event.hide(`oritech:${material}_ingot`)
-        event.hide(`oritech:${material}_plate`)
-        event.hide(`oritech:${material}_dust`)
-        event.hide(`oritech:${material}_nugget`)
-        event.hide(`oritech:raw_${material}`)
+        event.remove(`oritech:${material}_ingot`)
+        event.remove(`oritech:${material}_plate`)
+        event.remove(`oritech:${material}_dust`)
+        event.remove(`oritech:${material}_nugget`)
+        event.remove(`oritech:raw_${material}`)
 
         // --- ENDER IO ---
-        event.hide(`enderio:ingot_${material}`)
-        event.hide(`enderio:nugget_${material}`)
-        event.hide(`enderio:powder_${material}`) // Poudre/Dust
-        event.hide(`enderio:gear_${material}`)
+        event.remove(`enderio:ingot_${material}`)
+        event.remove(`enderio:nugget_${material}`)
+        event.remove(`enderio:powder_${material}`) 
+
+        // --- CREATE (Cas spécifiques) ---
+        if (material === 'zinc') {
+            event.remove('create:zinc_ingot')
+            event.remove('create:raw_zinc')
+            event.remove('create:zinc_nugget')
+        }
+        if (material === 'brass') {
+            event.remove('create:brass_ingot')
+            event.remove('create:brass_nugget')
+        }
+        event.remove(`create:${material}_sheet`)
     })
 
-    // --- EXTRAS ---
-    // Cacher les poudres vanilla doublons si EnderIO en ajoute
-    event.hide('enderio:powder_coal')
-    event.hide('enderio:powder_iron')
-    event.hide('enderio:powder_gold')
-    event.hide('enderio:powder_copper')
-    event.hide('enderio:powder_obsidian')
+    // Cacher les poudres vanilla doublons d'EnderIO
+    event.remove('enderio:powder_coal')
+    event.remove('enderio:powder_iron')
+    event.remove('enderio:powder_gold')
+    event.remove('enderio:powder_copper')
+    event.remove('enderio:powder_obsidian')
 })
