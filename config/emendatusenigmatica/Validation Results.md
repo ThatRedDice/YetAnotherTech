4d8e28cee7624ecb9a41f0002ff8e3ce7b5f29e2
TheRedDice <thereddestdice@gmail.com>
2026-01-28 15:47:07 +0100

    Fix unification.js

--- config/emendatusenigmatica/Validation Results.md
+++ config/emendatusenigmatica/Validation Results.md
@@ -2,7 +2,7 @@
 
 # Emendatus Enigmatica Validation Results
 Emendatus Enigmatica version: 1.21.1-2.2.0-alpha-3<br>
-File generated at: 28-01-2026 15:24:09.052<br>
+File generated at: 28-01-2026 15:36:50.581<br>
 <hr>
 
 ## Strata
@@ -15,7 +15,7 @@ All files were parsed and registered successfully!<br>
 All files were parsed and registered successfully!<br>
 ## Additional Information
 ### Performance
-<table><tr><td>Scanning and registration of addons</td><td>0.038s</td></tr><tr><td>Loading and parsing JSON Files</td><td>0.002s</td></tr><tr><td>Validation: Strata</td><td>0.000s</td></tr><tr><td>Validation: Material</td><td>0.000s</td></tr><tr><td>Validation: Compat</td><td>0.000s</td></tr><tr><td>Validation: Deposits</td><td>0.021s</td></tr><tr><td>Data Generation</td><td>0.191s</td></tr><tr><td>Validation: Registry</td><td>0.008s</td></tr><tr><td>Generation of Analytics Summary</td><td>0.019s</td></tr></table>
+<table><tr><td>Scanning and registration of addons</td><td>0.020s</td></tr><tr><td>Loading and parsing JSON Files</td><td>0.002s</td></tr><tr><td>Validation: Strata</td><td>0.000s</td></tr><tr><td>Validation: Material</td><td>0.000s</td></tr><tr><td>Validation: Compat</td><td>0.000s</td></tr><tr><td>Validation: Deposits</td><td>0.019s</td></tr><tr><td>Data Generation</td><td>0.186s</td></tr><tr><td>Validation: Registry</td><td>0.003s</td></tr><tr><td>Generation of Analytics Summary</td><td>0.018s</td></tr></table>
 
 
 > You can disable the generation of this summary and speed up the validation in the configuration file!
--- kubejs/server_scripts/unification.js
+++ kubejs/server_scripts/unification.js
@@ -1,41 +1,55 @@
 // Priority: 1000
 ServerEvents.recipes(event => {
     const materials = [
-        'iron', 'gold', 'copper', 
-        'zinc', 'brass', 
-        'tin', 'lead', 'silver', 'nickel', 'uranium', 'aluminum', 'steel',
+        'zinc', 'brass', 'tin', 'lead', 'silver', 'nickel', 'uranium', 'aluminum', 'steel',
         'bronze', 'electrum', 'constantan', 'invar', 'osmium'
     ]
 
+    // On traite les métaux MODDÉS (tout sauf fer/or/cuivre)
     materials.forEach(material => {
-        // --- 1. INPUTS (Remplacement intelligent) ---
-        // On remplace UNIQUEMENT si l'item existe pour éviter le crash
+        // Remplacer les entrées par des TAGS
+        event.replaceInput({}, `create:${material}_ingot`, `#c:ingots/${material}`)
+        event.replaceInput({}, `immersiveengineering:ingot_${material}`, `#c:ingots/${material}`)
+        event.replaceInput({}, `oritech:${material}_ingot`, `#c:ingots/${material}`)
+        event.replaceInput({}, `enderio:ingot_${material}`, `#c:ingots/${material}`)
         
-        // Create (Ignore Fer/Or/Cuivre car Create ne les a pas)
-        if (!['iron', 'gold', 'copper'].includes(material)) {
-            if (Item.of(`create:${material}_ingot`).id != 'minecraft:air') {
-                event.replaceInput({}, `create:${material}_ingot`, `#c:ingots/${material}`)
-            }
-        }
-        
-        // Immersive Engineering
-        if (Item.of(`immersiveengineering:ingot_${material}`).id != 'minecraft:air') {
-            event.replaceInput({}, `immersiveengineering:ingot_${material}`, `#c:ingots/${material}`)
-        }
-
-        // --- 2. OUTPUTS (Sortie forcée vers Emendatus Enigmatica) ---
-        // On ne touche pas aux lingots vanilla
-        if (!['iron', 'gold', 'copper'].includes(material)) {
-            // Vérifie que l'item EE existe avant de forcer, sinon on garde l'original
-            // C'est la sécurité anti-crash "Steel Ingot missing"
-            if (Item.of(`emendatusenigmatica:${material}_ingot`).id != 'minecraft:air') {
-                event.replaceOutput({}, `#c:ingots/${material}`, `emendatusenigmatica:${material}_ingot`)
-                event.replaceOutput({}, `#c:nuggets/${material}`, `emendatusenigmatica:${material}_nugget`)
-                event.replaceOutput({}, `#c:plates/${material}`, `emendatusenigmatica:${material}_plate`)
-                event.replaceOutput({}, `#c:dusts/${material}`, `emendatusenigmatica:${material}_dust`)
-                event.replaceOutput({}, `#c:gears/${material}`, `emendatusenigmatica:${material}_gear`)
-                event.replaceOutput({}, `#c:rods/${material}`, `emendatusenigmatica:${material}_rod`)
-            }
+        // Remplacer les sorties par EMENDATUS ENIGMATICA
+        // On vérifie d'abord que l'item cible existe (au cas où l'acier n'est pas là)
+        if (Item.of(`emendatusenigmatica:${material}_ingot`).id != 'minecraft:air') {
+            event.replaceOutput({}, `#c:ingots/${material}`, `emendatusenigmatica:${material}_ingot`)
+            event.replaceOutput({}, `#c:nuggets/${material}`, `emendatusenigmatica:${material}_nugget`)
+            event.replaceOutput({}, `#c:plates/${material}`, `emendatusenigmatica:${material}_plate`)
+            event.replaceOutput({}, `#c:dusts/${material}`, `emendatusenigmatica:${material}_dust`)
+            event.replaceOutput({}, `#c:gears/${material}`, `emendatusenigmatica:${material}_gear`)
+            event.replaceOutput({}, `#c:rods/${material}`, `emendatusenigmatica:${material}_rod`)
         }
     })
+
+    // --- CAS SPÉCIAUX : FER, OR, CUIVRE ---
+    // Pour eux, on ne touche qu'aux PLAQUES et POUSSIÈRES, pas aux lingots !
+    const vanillaMaterials = ['iron', 'gold', 'copper']
+    
+    vanillaMaterials.forEach(material => {
+        // Unification des PLAQUES (Vital pour Create/IE)
+        event.replaceInput({}, `create:${material}_sheet`, `#c:plates/${material}`)
+        event.replaceInput({}, `immersiveengineering:plate_${material}`, `#c:plates/${material}`)
+        
+        if (Item.of(`emendatusenigmatica:${material}_plate`).id != 'minecraft:air') {
+             event.replaceOutput({}, `#c:plates/${material}`, `emendatusenigmatica:${material}_plate`)
+        }
+
+        // Unification des POUSSIÈRES (Vital pour EnderIO/Mekanism)
+        event.replaceInput({}, `immersiveengineering:dust_${material}`, `#c:dusts/${material}`)
+        event.replaceInput({}, `enderio:powder_${material}`, `#c:dusts/${material}`) // EnderIO appelle ça powder
+
+        if (Item.of(`emendatusenigmatica:${material}_dust`).id != 'minecraft:air') {
+             event.replaceOutput({}, `#c:dusts/${material}`, `emendatusenigmatica:${material}_dust`)
+        }
+    })
+
+    // Correction spécifique Create : Sheet -> Plate
+    event.replaceOutput({mod: 'create'}, 'create:brass_sheet', 'emendatusenigmatica:brass_plate')
+    event.replaceOutput({mod: 'create'}, 'create:copper_sheet', 'emendatusenigmatica:copper_plate')
+    event.replaceOutput({mod: 'create'}, 'create:iron_sheet', 'emendatusenigmatica:iron_plate')
+    event.replaceOutput({mod: 'create'}, 'create:golden_sheet', 'emendatusenigmatica:gold_plate')
 })
