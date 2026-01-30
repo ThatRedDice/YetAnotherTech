7be58b54adf9fbdab1fb72e49f445ada9bbe8744
ThatRedDice <thereddestdice@gmail.com>
2026-01-30 17:23:47 +0100

    Added materials

---
+++ config/emendatusenigmatica/default/data/emendatusenigmatica/material/steel.js
@@ -0,0 +1,12 @@
+{
+  "id": "emendatusenigmatica:steel",
+  "source": "vanilla",
+  "localizedName": "Steel",
+  "processedType": "ingot",
+  "color": "565656",
+  "properties": {
+    "materialType": "alloy",
+    "harvestLevel": "iron"
+  },
+  "strata": []
+}
